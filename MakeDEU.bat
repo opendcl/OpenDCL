d90486307a410c56cef8e52fb0a2ee029fe0ef72
Owen Wengerd <owenw@manusoft.com>
2009-02-23 07:12:59 +0000

    git-svn-id: https://svn.code.sf.net/p/opendcl/code/Trunk@136 07a8c159-f786-4120-a571-e88efff3e5ae

--- MakeDEU.bat
+++
@@ -1,4 +0,0 @@
-robocopy "<ODCL5>\Studio\DEU\Content" "<TEMPFOLDER>" /mir /xd debug* release* .svn _* /xf *.zip thumbs.db *.apf *.reg *.tif
-@set PATH=%PATH%;C:\Program Files\WinZip
-del OpenDCLDEU.zip
-wzzip OpenDCLDEU.zip "<TEMPFOLDER>\*.*" -r -p -m
