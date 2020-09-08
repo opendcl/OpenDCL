cd3dd6ced7ef59715cee2dfa4db9a9e68ea36518
Owen Wengerd <owenw@manusoft.com>
2020-09-08 01:24:05 +0000

    OpenDCL 9.0.6.1 - Added recently added new files that were accidentally omitted
    git-svn-id: https://svn.code.sf.net/p/opendcl/code/Trunk@378 07a8c159-f786-4120-a571-e88efff3e5ae

---
+++ Runtime/GRX.2020/VI/ARXVI.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#define _ACADTARGET 19
+
+#pragma comment(linker, "/EXPORT:odrxCreateModuleObject=_odrxCreateModuleObject")
+#pragma comment(linker, "/EXPORT:odrxGetAPIVersion=_odrxGetAPIVersion")
+
+
+//custom macro for use with command names that include special characters (e.g. hyphens)
+#define ACED_ARXCOMMAND_ENTRY_KEY_AUTO(T_CLASS,T_KEY,T_GROUPNAME,T_GLOBALNAME,T_LOCALNAME,T_FLAGS,T_CONTEXT) \
+  _GRXCOMMAND_ENTRY __GrxCmdMap_##T_GROUPNAME##T_KEY = { L#T_GROUPNAME, L#T_KEY, L#T_LOCALNAME, T_FLAGS, classname::##T_GROUPNAME ##T_GLOBALNAME, T_CONTEXT, (UINT)-1 , NULL} ; \
+  _CGcRxAddCommandEntryAuto __GrxAddCommandAuto_##T_GROUPNAME##T_KEY(& __GrxCmdMap_##T_GROUPNAME##T_KEY) ;
+
+//overwrite GRX version of macros that prefix "gds_" to command handler names instead of "ads_"
+#include "acad2grx.h"
+#undef ACED_ADSSYMBOL_ENTRY_AUTO
+#define ACED_ADSSYMBOL_ENTRY_AUTO(classname, name, regFunc) \
+  _GDSSYMBOL_ENTRY __GdsSymbolMap_##name = { L#name, classname::ads_ ##name, regFunc, (UINT)-1 , NULL } ; \
+  _CGcRxAddSymbolEntryAuto __GrxAddSymbolAuto_##name(& __GdsSymbolMap_##name) ;
+
+#undef ACED_ADSCOMMAND_ENTRY_AUTO
+#define ACED_ADSCOMMAND_ENTRY_AUTO(classname, name, regFunc) \
+  _GDSSYMBOL_ENTRY __GdsSymbolMap_##name = { L"C:" L#name, classname::ads_ ##name, regFunc, (UINT)-1 , NULL } ; \
+  _CGcRxAddSymbolEntryAuto __GrxAddSymbolAuto_##name(& __GdsSymbolMap_##name) ;
+
+#undef ACED_ADSSYMBOL_ENTRYBYID_AUTO
+#define ACED_ADSSYMBOL_ENTRYBYID_AUTO(classname, name, nameId, regFunc) \
+  _GDSSYMBOL_ENTRY __GdsSymbolMap_##name = { NULL, classname::ads_ ##name, regFunc, nameId , NULL } ; \
+  _CGcRxAddSymbolEntryAuto __GrxAddSymbolAuto_##name(& __GdsSymbolMap_##name) ;
+
+#define kOnDWGdirectStartup kOnTeighaStartup
